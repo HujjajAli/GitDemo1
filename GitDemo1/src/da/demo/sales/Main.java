51ad83f0f363815c72df7f073a254e6402ac69fd
HujjajAli <hujjajali@gmail.com>
2019-07-02 16:53:04 +0500

    i am doing this

---
+++ GitDemo1/src/da/demo/sales/Main.java
@@ -0,0 +1,12 @@
+package da.demo.sales;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData d = new SalesData();
+		d.display();
+		System.out.println("SALE SALE SALE");
+
+	}
+
+}
---
+++ GitDemo1/src/da/demo/sales/SalesData.java
@@ -0,0 +1,12 @@
+package da.demo.sales;
+
+public class SalesData {
+	
+	int  data[] = {2,4,6};
+
+	public void display() {
+		System.out.print("Happy Sales");
+		
+	}
+
+}
